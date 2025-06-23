6b3e808c9c8ee0ef59287fa2835d78203a5a50c7
TANISH GUPTA <118546416+tanish-hrk@users.noreply.github.com>
2025-06-23 09:17:37 +0530

    Time: 2 ms (90.81%), Space: 45.6 MB (93.32%) - LeetHub

--- 1720-decode-xored-array/1720-decode-xored-array.java
+++ 1720-decode-xored-array/1720-decode-xored-array.java
@@ -3,8 +3,10 @@ class Solution {
         int n = encoded.length;
         int[] ans = new int[n + 1];
         ans[0] = first;
+        int idx=0;
         for (int i = 1; i < n + 1; i++) {
-            ans[i] = ans[i - 1] ^ encoded[i - 1];
+            ans[i] = ans[i - 1] ^ encoded[idx];
+            idx++;
         }
         return ans;
     }
