99b44fbcf2e2369d1bc7943d238c1f3bcb611139
TANISH GUPTA <118546416+tanish-hrk@users.noreply.github.com>
2025-06-26 09:42:56 +0530

    Time: 14 ms (61.6%), Space: 48.5 MB (6.42%) - LeetHub

---
+++ 0542-01-matrix/0542-01-matrix.java
@@ -0,0 +1,52 @@
+class Solution {
+    public int[][] updateMatrix(int[][] mat) {
+        int m = mat.length;
+        int n = mat[0].length;
+        int[][] dp = new int[m][n];
+
+        // Initialize dp with distances
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (mat[i][j] == 0) {
+                    dp[i][j] = 0;
+                } else {
+                    dp[i][j] = Integer.MAX_VALUE - 100000; // Prevent overflow
+                }
+            }
+        }
+
+        fromTopLeft(dp, m, n);
+        fromBottomRight(dp, m, n);
+        return dp;
+    }
+
+    void fromTopLeft(int[][] dp, int m, int n) {
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (dp[i][j] != 0) {
+                    if (i > 0) {
+                        dp[i][j] = Math.min(dp[i][j], dp[i - 1][j] + 1);
+                    }
+                    if (j > 0) {
+                        dp[i][j] = Math.min(dp[i][j], dp[i][j - 1] + 1);
+                    }
+                }
+            }
+        }
+    }
+
+    void fromBottomRight(int[][] dp, int m, int n) {
+        for (int i = m - 1; i >= 0; i--) {
+            for (int j = n - 1; j >= 0; j--) {
+                if (dp[i][j] != 0) {
+                    if (i < m - 1) {
+                        dp[i][j] = Math.min(dp[i][j], dp[i + 1][j] + 1);
+                    }
+                    if (j < n - 1) {
+                        dp[i][j] = Math.min(dp[i][j], dp[i][j + 1] + 1);
+                    }
+                }
+            }
+        }
+    }
+}
