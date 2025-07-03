d4e79b314f903481ab353861ee72a95f55825a6c
TANISH GUPTA <118546416+tanish-hrk@users.noreply.github.com>
2025-07-03 09:39:19 +0530

    Time: 0 ms (100%), Space: 40.4 MB (81.35%) - LeetHub

---
+++ 1823-find-the-winner-of-the-circular-game/1823-find-the-winner-of-the-circular-game.java
@@ -0,0 +1,10 @@
+class Solution {
+    public int findTheWinner(int n, int k) {
+        return josephus(n, k) + 1;
+    }
+
+    private int josephus(int n, int k) {
+        if (n == 1) return 0;
+        return (josephus(n - 1, k) + k) % n;
+    }
+}
