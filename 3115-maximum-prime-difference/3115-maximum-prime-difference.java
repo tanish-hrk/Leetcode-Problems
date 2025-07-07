edb38b9fb855731ae6baf16e320da511237cd5a8
TANISH GUPTA <118546416+tanish-hrk@users.noreply.github.com>
2025-07-07 10:12:32 +0530

    Time: 1 ms (99.55%), Space: 79.5 MB (40.58%) - LeetHub

--- 3115-maximum-prime-difference/3115-maximum-prime-difference.java
+++ 3115-maximum-prime-difference/3115-maximum-prime-difference.java
@@ -1,30 +1,31 @@
 class Solution {
     public int maximumPrimeDifference(int[] nums) {
-        int start = 0;
-        int end = nums.length - 1;
+        int first = -1;
+        int last = -1;
 
-        // the index of the first prime
-        while (start < end) {
-            if (isPrime(nums[start])) {
+        // Find the first prime's index
+        for (int i = 0; i < nums.length; i++) {
+            if (isPrime(nums[i])) {
+                first = i;
                 break;
             }
-            start++;
         }
 
-        // the index of the last prime
-        while (end >= start) {
-            if (isPrime(nums[end])) {
+        // Find the last prime's index
+        for (int i = nums.length - 1; i >= 0; i--) {
+            if (isPrime(nums[i])) {
+                last = i;
                 break;
             }
-            end--;
         }
 
-        return end - start;
+        // Return the difference if primes found
+        return (first != -1 && last != -1) ? (last - first) : 0;
     }
 
     private boolean isPrime(int n) {
         if (n < 2) return false;
-        for (int i = 2; i*i <= n; i++) {
+        for (int i = 2; i * i <= n; i++) {
             if (n % i == 0) return false;
         }
         return true;
