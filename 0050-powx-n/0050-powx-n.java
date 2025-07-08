7c714f52ca7bf39eae07df3a63c1032fb642a592
TANISH GUPTA <118546416+tanish-hrk@users.noreply.github.com>
2025-07-08 09:32:30 +0530

    Time: 0 ms (100%), Space: 42.8 MB (26.41%) - LeetHub

--- 0050-powx-n/0050-powx-n.java
+++ 0050-powx-n/0050-powx-n.java
@@ -1,7 +1,16 @@
 class Solution {
     public double myPow(double x, int n) {
-        double c = Math.pow(x,n);
-        return c;
-        
+       if(n == 0)
+            return 1;
+
+        if(n == Integer.MIN_VALUE){
+            x = x * x;
+            n = n/2;
+        }
+
+        if(n<0){
+            return 1/myPow(x,-n);
+        }
+        return (n%2 == 0) ? myPow(x*x, n/2) : x*myPow(x*x, n/2);
     }
 }
