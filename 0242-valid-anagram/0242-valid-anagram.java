52df416cd605a1cc0785cba887597ad367e1ae9c
TANISH GUPTA <118546416+tanish-hrk@users.noreply.github.com>
2025-09-18 14:41:58 +0530

    Time: 6 ms (42.11%), Space: 43.2 MB (71.52%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.java
+++ 0242-valid-anagram/0242-valid-anagram.java
@@ -1,16 +1,19 @@
 class Solution {
     public boolean isAnagram(String s, String t) {
-        if(s.length() != t.length()) return false;
-        char[] count = new char[26];
-
-        for(int i=0;i<s.length();i++){
+        if(s.length()!= t.length()){
+            return false;
+        }
+        int[] count = new int[26];
+        for(int i = 0 ; i<s.length();i++){
             count[s.charAt(i)-'a']++;
             count[t.charAt(i)-'a']--;
         }
-
-        for(int c : count){
-            if(c!=0) return false;
+        for(int i =0 ;i<count.length;i++){
+            if(count[i]!=0){
+                return false;
+            }
         }
         return true;
-    }
+    } 
+        
 }
